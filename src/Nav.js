d734389e868dd47cc51dd649646b1e2354a361a1
Kayla <lucekl03@frontier.com>
2025-09-24 11:28:19 -0400

    Initial creation of nav bar in project

---
+++ src/Nav.js
@@ -0,0 +1,11 @@
+
+function Nav () {
+    return ( 
+  <div>
+    <a href="link">SKINSTRIC</a>
+    <button className="nav-button">ENTER CODE</button>
+    <p>INTRO</p>
+  </div>
+    )
+}
+export default Nav;
