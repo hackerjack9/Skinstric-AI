960371cb5753694c050340baf84eb41fc1c22e99
Kayla <lucekl03@frontier.com>
2025-09-24 21:16:08 -0400

    Added styling and html to nav and home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,15 +1,40 @@
 import React from 'react';
-
-
+import LeftTriangle from "../assets/Shapes/Rectangle 2779.png"
+import LeftArrowIcon from "../assets/Shapes/button-icon-text-shrunk (1).svg"
+import RightArrowIcon from "../assets/Shapes/button-icon-text-shrunk (2).svg"
+import RightTriangle from "../assets/Shapes/Rectangle 2778.svg"
 function Home() {
 
-   
   return (
     <div className='main-heading'>
-    <h1 className='header-title'>Sophisticated</h1>
-        <span className='header-subtitle'>skincare</span>
-        <button className='discover-button'><span>Discover A.I.</span></button>
+    <h1 className='header-title'>Sophisticated
+  <br />
+       <span className='header-subtitle'>skincare</span>
+    </h1>
+  
+
+<div id='left-section'>
+   <div className='left-rectangle'>
+       <div className='left-triangle'><img src={LeftTriangle} alt="" />
+        <button className='discover-button'>
+          <div className='mini-box'></div>
+          <span className='discover-arrow'><img src={LeftArrowIcon} alt="" /></span>
+      </button>
         </div>
+      </div>
+      <div id='right-section'>
+        <div className='right-rectangle'>
+          <div className='right-triangle'><img src={RightTriangle} alt="" /></div>
+          <button className='test-button'>
+          <div className='mini-box'></div>
+          <span className='test-arrow'><img src={RightArrowIcon} alt="" /></span>
+       </button>
+        </div>
+      </div>
+       </div>
+       </div>
+
+       
        
   )
 }
